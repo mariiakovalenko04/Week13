577615bfee1f5dfbcb25f7f144c61ab7deb4d96a
mariiakovalenko04 <mkovalenko@berklee.edu>
2025-04-24 08:55:00 -0400

    Level2Finished

---
+++ Level2.py
@@ -0,0 +1,7 @@
+#mix_of_stuff = ["pencil", "True", "1", "1.4"]
+#print (mix_of_stuff)
+
+#music_playlist = ["Cherokee", "Paranoia Purple", "Laughing In Her Sleep", "Dawn Dreamer", "The Song Is You"]
+#music_playlist.append("Love You Madly")
+#print (music_playlist[5])
+
